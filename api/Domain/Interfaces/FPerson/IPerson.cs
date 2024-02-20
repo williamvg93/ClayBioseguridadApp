3d856ce418fefc7805116082f2c67a2bfcad93d5
williamvg93 <sonico_5071@hotmail.com>
2024-02-20 12:54:03 -0500

    feat: :sparkles: The interface for the generic repository was created, all the interfaces for each entity were added
    In the domain folder the interfaces folder was added, the interfaces for the generic repository and the entities were added

---
+++ api/Domain/Interfaces/FPerson/IPerson.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities.FPerson;
+
+namespace Domain.Interfaces.FPerson;
+
+public interface IPerson : IGenericRepository<Person>
+{
+}
