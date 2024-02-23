15261aa1f13e7a7ab0896d11dd1f8a8760d72c5d
williamvg93 <sonico_5071@hotmail.com>
2024-02-22 20:48:55 -0500

    feat: :sparkles: The first migration was created, the database was updated, the dbcontext was added in program

--- api/ApiClayBio/Program.cs
+++ api/ApiClayBio/Program.cs
@@ -1,9 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Persitence.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+/* Add connection to database */
+builder.Services.AddDbContext<ApiClayBioContext>(options =>
+{
+    string connectionStrings = builder.Configuration.GetConnectionString("MysqlConnect");
+    options.UseMySql(connectionStrings, ServerVersion.AutoDetect(connectionStrings));
+});
+
 
 var app = builder.Build();
 
@@ -12,6 +24,22 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    try
+    {
+        var context = services.GetRequiredService<ApiClayBioContext>();
+        await context.Database.MigrateAsync();
+    }
+    catch (Exception ex)
+    {
+        var _logger = loggerFactory.CreateLogger<Program>();
+        _logger.LogError(ex, "Ocurrio un error durante la migracion !!");
+    }
 }
 
 app.UseHttpsRedirection();
